0fdd5ad866cd9126c07d0271d7a0ff6db0fd937e
Ivan Horbynko <horbynko.i@gmail.com>
2022-11-23 21:47:07 +0200

    add SidebarHeader

--- src/features/sidebar/SidebarHeader/SidebarHeader.tsx
+++ src/features/sidebar/SidebarHeader/SidebarHeader.tsx
@@ -1,1 +1,12 @@
-export const SidebarHeader = (): JSX.Element => <div>SidebarHeader</div>;
+import { Box, ListItem } from "@mui/material";
+
+export const SidebarHeader = (): JSX.Element => (
+  <ListItem divider>
+    <Box
+      component="img"
+      src="/static/images/school_logo.svg"
+      alt="school logo"
+      sx={{ maxWidth: 84, height: 56 }}
+    />
+  </ListItem>
+);
