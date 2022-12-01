31b6dcc8f666e2adb2785e87b01a5d788c1f3d95
Ivan Horbynko <horbynko.i@gmail.com>
2022-12-01 10:44:20 +0200

    create AuthLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,10 +1,39 @@
-import * as React from "react";
 import { Outlet } from "react-router-dom";
+import { Grid, Box, CardMedia, Stack } from "@mui/material";
 
-export const AuthLayout = (): JSX.Element => {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
-};
+export const AuthLayout = () => (
+  <Box component="main">
+    <Grid container sx={{ flex: "1 1 auto", height: "100vh" }}>
+      <Grid item xs={12} lg={7}>
+        <CardMedia
+          height="100%"
+          component="img"
+          image="/static/images/showcase/showcase.jpg"
+          alt="showcase"
+        />
+      </Grid>
+      <Grid item xs={12} lg={5}>
+        <Stack
+          sx={{
+            maxWidth: "308px",
+            height: "100%",
+            margin: "0 auto",
+          }}>
+          <CardMedia
+            component="img"
+            image="/static/logo.svg"
+            alt="logo"
+            sx={{ maxWidth: "112px", height: "auto" }}
+          />
+          <Box
+            sx={{
+              mt: "130px",
+              height: "100%",
+            }}>
+            <Outlet />
+          </Box>
+        </Stack>
+      </Grid>
+    </Grid>
+  </Box>
+);
