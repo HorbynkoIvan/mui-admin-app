60c208400e80ddd52e3033dcf9f0c2d401a68390
Ivan Horbynko <horbynko.i@gmail.com>
2023-03-01 16:20:30 +0200

    added Dashboard

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -81,6 +81,8 @@ export const useMode = () => {
     []
   );
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const theme: any = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return [theme, colorMode];
